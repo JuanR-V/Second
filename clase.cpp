605a03fcbf2685f8eb873bc2aa3cbf2ae67e1a08
Juan Rivera Vargas <71957386+JuanR-V@users.noreply.github.com>
2021-05-01 18:45:54 -0500

    Create clase.cpp
    Clase en C++

---
+++ clase.cpp
@@ -0,0 +1,12 @@
+//JUAN RIVERA VARGAS
+//DERIVACION MULTIPPLE
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+class Vertebrado{
+    
+    public:
+        void InfGeneral();
+};
