a2c166403f44d1c91da2537dd839db12489222cc
Juan Rivera Vargas <71957386+JuanR-V@users.noreply.github.com>
2021-06-21 14:28:37 -0500

    Update clase.cpp

--- clase.cpp
+++ clase.cpp
@@ -1,19 +1,51 @@
 //JUAN RIVERA VARGAS
-//DERIVACION MULTIPPLE
+//Creacion de una clase Persona
 #include <iostream>
 #include <stdlib.h>
-
 using namespace std;
-
-class Vertebrado{
-    
-    public:
-        void InfGeneral();
+//clase
+class Persona{
+    private://ATRIBUTOS
+        int edad;
+        string nombre;
+    public://METODOS, son los unicos que pueden
+        void leer();//modificar los atributos
+        void correr();
+        Persona(int,string);//constructor, ayuda a 
+        					//inicializar los atributos
 };
+//Constructor
+//es el encargado de inicializar los atributos
+//El nombre debe ser el mismo que en su declaracion dentro de la clase persona
+Persona::Persona(int _edad, string _nombre){
+    edad= _edad;
+    nombre=_nombre;
+}
 
+//Metodos de la clase
+//sintaxis
+//tipo AqueClasePertenece :: nombre(){}
 
-int main(){
-    print("hola")
+//metodo leer
+void Persona::leer(){
+    cout<<"Soy "<<nombre<<" y estoy leyendo"<<endl<<endl;
+}
+//metodo correr
+void Persona::correr(){
+    cout<<"Soy "<<nombre<<" y estoy corriendo"
+        <<" Tengo "<<edad<<" anios"<<endl<<endl;
+}
+
+int main()
+{
+    Persona p1 =Persona(20,"Juan");//creacion o instancia del objeto p1
+    Persona p2 =Persona(21,"Raul");//creacion o instancia del objeto p2
+    Persona p3(21,"Maria");//creacion o instancia del objeto p3
+    
+    p1.leer();//metodo del objeto p1
+    p2.correr();//metodo del objeto p2
+    p3.leer();//metodos del objeto p3
+    p3.correr();
+    
     return 0;
- 
 }
